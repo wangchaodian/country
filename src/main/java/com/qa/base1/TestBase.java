76e9ce18758feee3099cced1661f12bab654a6a0
wangchao <wangchao@sunsharp.cn>
2020-09-10 19:41:41 +0800

    修复文件流路径错误

--- src/main/java/com/qa/base1/TestBase.java
+++ src/main/java/com/qa/base1/TestBase.java
@@ -21,7 +21,8 @@ public class TestBase {
 		
 		try {
 			prop = new Properties();
-			FileInputStream fis = new FileInputStream(System.getProperty("D:\\JenkinsNode\\apidata\\config.properties"));
+			FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+
+"\\apidata\\config.properties");
 			prop.load(fis);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
